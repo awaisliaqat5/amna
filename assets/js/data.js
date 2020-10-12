5bdd8a69e102fa1668c36cb27b7abb8e011e9046
thedoctordesign <awaisliaqat5@gmail.com>
2020-10-12 22:58:48 +0500

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,26 @@
+let obj = [
+	{
+		"name": "Amna",
+		"email": "amna@gmail.com",
+		"dob": "20-10-2020",
+		"phone": "03001234567"
+	},
+	{
+		"name": "Sister",
+		"email": "sister@gmail.com",
+		"dob": "10-08-1995",
+		"phone": "03009876543"
+	},
+	{
+		"name": "Brother",
+		"email": "brother@gmail.com",
+		"dob": "10-08-1995",
+		"phone": "03009876543"
+	},
+	{
+		"name": "Uncle",
+		"email": "uncle@gmail.com",
+		"dob": "10-08-1992",
+		"phone": "03129876540"
+	}
+];
